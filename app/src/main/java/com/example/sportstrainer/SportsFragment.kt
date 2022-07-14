429819a938748244cdee72d94bb9367033eca85e
JasonSmith03 <39653254+JasonSmith03@users.noreply.github.com>
2022-07-13 20:24:59 -0400

    conventional naming for SportsFragment

--- app/src/main/java/com/example/sportstrainer/SportsFragment.kt
+++ app/src/main/java/com/example/sportstrainer/SportsFragment.kt
@@ -21,7 +21,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [sports.newInstance] factory method to
  * create an instance of this fragment.
  */
-class sports : Fragment() {
+class SportsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
